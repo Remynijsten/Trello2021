abb131a0af3516d71d640897d7548bc1f7819082
Remy Nijsten <99012252@mydavinci.nl>
2021-02-17 10:48:22 +0100

    Added htaccess and routing switch

--- index.php
+++ index.php
@@ -6,7 +6,31 @@ define('ROOT', '../checkmate/');
 include 'templates/head.php';
 
 include 'templates/header-default.php';
-include 'templates/pages/home.php';
+
+
+/** Returns and cleans request URI string */
+$route = str_replace('/checkmate', '', $_SERVER['REQUEST_URI']);
+
+/**	
+*	URI Routing switch determines which php content to load per URL
+* 	Sets a single-line title.
+* 	@param string $route Page request title. For example: 'register'
+* 	@return Includes related content php files
+**/
+switch ($route) {
+
+	case '/':
+		include 'templates/pages/home.php';
+		break;
+
+    case '/register' :
+        include 'templates/pages/register.php';
+        break;
+    
+    default:
+        include 'templates/pages/404.php';
+        break;
+}
 
 
 include 'templates/footer.php';
