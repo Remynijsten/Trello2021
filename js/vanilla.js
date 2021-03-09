f0293109ab601c58c608fe8163070cb1dc50600c
Remy Nijsten <99012252@mydavinci.nl>
2021-03-09 12:08:02 +0100

    Removed large ajax functional & created compact solution

--- js/vanilla.js
+++ js/vanilla.js
@@ -1,26 +1,3 @@
-function ajax(model, method, params, callback){
-	params = 'data=' + JSON.stringify(params);
-
-	var xhr = new XMLHttpRequest();
-	xhr.open(method, './model/' + model + '.php');
-	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-	xhr.onload = function() {
-	    if (xhr.status === 200) {
-	        callback(xhr.responseText);
-	    }
-	    else{
-	        return false;
-	    }
-	};
-	
-	if(params != ''){
-		xhr.send(params);
-	} else {
-		xhr.send(null);
-	}
-}
-
 function display_message(data){
 	let message = data[0];
 	let mode 	= data[1];
